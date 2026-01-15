b94a58bafe2a6c5d30022c8bc0158f385f77a4f7
evan <frawleyevan@gmail.com>
2026-01-15 13:23:57 -0600

    Create projects.js

---
+++ assets/js/projects.js
@@ -0,0 +1,42 @@
+window.PROJECTS = [
+  {
+    id: "finland",
+    title: "Art & Social Sustainability: Public Spaces That Inspire",
+    lat: 60.17744444444444,
+    lng: 24.92025,
+    image: "https://frawleyevan9.wordpress.com/wp-content/uploads/2026/01/img_9917.jpg",
+    page: "writing.html#finland"
+  },
+  {
+    id: "marburg",
+    title: "Cycling Safety Perceptions in Marburg, Germany: A Spatial Analysis",
+    lat: 50.80775,
+    lng: 8.772333333333334,
+    image: "https://frawleyevan9.wordpress.com/wp-content/uploads/2026/01/marburg-03.jpg",
+    page: "maps.html#marburg"
+  },
+  {
+    id: "arizona",
+    title: "Car-Free in the Capital of Sprawl: The Trade-Offs of Living in Culdesac Tempe",
+    lat: 33.41408333333333,
+    lng: -111.89933333333335,
+    image: "https://frawleyevan9.wordpress.com/wp-content/uploads/2026/01/img_1080.jpg",
+    page: "writing.html#arizona"
+  },
+  {
+    id: "manitowoc",
+    title: "River Point District Project Proposal",
+    lat: 44.09505555555556,
+    lng: -87.66352777777779,
+    image: "https://frawleyevan9.wordpress.com/wp-content/uploads/2026/01/screenshot-2026-01-14-184127.png",
+    page: "writing.html#manitowoc"
+  },
+  {
+    id: "madison",
+    title: "Beyond Decibels: A Soundscape-Based Policy Framework for Urban Noise Pollution in Madison, Wisconsin",
+    lat: 43.034555555555556,
+    lng: -89.43697222222222,
+    image: "https://frawleyevan9.wordpress.com/wp-content/uploads/2026/01/geog280final4.png",
+    page: "sound.html#madison"
+  }
+];
